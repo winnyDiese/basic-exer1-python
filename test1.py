8b8c821aba704319ac3abbf683ffba4479254dec
winnyDiese <winnydiese@gmail.com>
2022-10-16 15:48:13 +0200

    first commit

---
+++ test1.py
@@ -0,0 +1,16 @@
+
+# Variable 
+name = "Pascal"
+
+# tableau
+family = ["Pasco", "Synt", "Schelu", "Schelo", "Jysch"]
+# print(family[0])
+
+
+# Condition
+if family[0] == "Pascal" : 
+    print("You are not the family")
+elif family[1] == "Synt":
+    print("Hello Synt, how are you ?")
+else :
+    print("Give me your name, please")
